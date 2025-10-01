1799d7e8b56d67c154fbfc4cff3f1515874ab88e
L0C3Y <yash2230awm@gmail.com>
2025-10-01 22:00:46 +0530

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // ✅ Replace with your real Supabase project values
-const supabaseUrl = "https://YOUR_PROJECT_ID.supabase.co"; // quotes are required
-const supabaseAnonKey = "YOUR_ANON_KEY";                   // quotes are required
+const supabaseUrl = "https://wqwmzwsvgbqouoponuku.supabase.co"; // quotes required
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxd216d3N2Z2Jxb3VvcG9udWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NTQ5NzAsImV4cCI6MjA3MjEzMDk3MH09XByssM-ejdmz_nAYOZn4KmVc4vb-ILnKVMOLp3Oe9s"; // quotes required
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
